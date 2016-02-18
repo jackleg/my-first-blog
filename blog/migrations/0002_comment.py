37f5fa9f72272e647ceec9683b945374d5cca899
한승균 <sk.han@daumkakao.com>
2016-02-18 23:29:08 +0900

    Django girls extension 학습.

---
+++ blog/migrations/0002_comment.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=datetime.datetime(2016, 2, 18, 13, 49, 18, 197363, tzinfo=utc))),
+                ('approved_comment', models.BooleanField(default=False)),
+                ('post', models.ForeignKey(related_name='comments', to='blog.Post')),
+            ],
+        ),
+    ]
--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -106,3 +106,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+LOGIN_REDIRECT_URL = '/'
